2513e8ddc14b05822c552e038fd6d4dbf0e99a94
PrabirKalwani <prabir.kalwani@gmail.com>
2022-12-17 12:27:42 +0530

    main push
    jaldi wala push

---
+++ Fonts/breymont-2022-02-08-22-29-24-utc/Breymont_v2/README.txt
@@ -0,0 +1,25 @@
+
+
+Licenses :
+https://elements.envato.com/license-terms
+
+------------------------------------------------
+
+Breymont Font
+
+Solid, Elegant, and Exquisite. Breymont is the new modern all caps typeface that always feels right under any circumstances. The dimensions of the letters and the general sense are modern and contemporary but also retain the refined functional touch of the rational structure. The result is a unique and unorthodox typeface that is perfectly suited for graphic design application ranging from editorial and corporate design to web and interaction design.
+
+
+Thanks for purchasing and I hope you have fun with Breymont!
+
+~ Hindia
+
+
+------------------------------------------------
+
+Support
+
+Mail support : hindia.studio@gmail.com / tulusdriyo@gmail.com
+Facebook : https://www.facebook.com/hindiastudio
+Dribbble : dribbble.com/hindiastudio / dribbble.com/tulusdriyo
+
---
+++ main.js
@@ -0,0 +1,25 @@
+
+// this takes the date which will be set to cyber cypher date now set to jan 20
+const countDownDateTime = new Date(2023, 1,20, 0, 0, 0).getTime();
+// days and time 
+const daysValue = document.querySelector("#days");
+const hoursValue = document.querySelector("#hours");
+const minutesValue = document.querySelector("#minutes");
+const secondsValue = document.querySelector("#seconds");
+// run this function every 1000 ms or 1 second
+let x = setInterval(function () {
+ const dateTimeNow = new Date().getTime();
+ let difference = countDownDateTime - dateTimeNow;
+ // calculating time and assigning values
+ daysValue.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
+ hoursValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+ );
+ minutesValue.innerHTML = Math.floor(
+ (difference % (1000 * 60 * 60)) / (1000 * 60)
+ );
+ secondsValue.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+ if (difference < 0) {
+ clearInterval(x);
+ }
+}, 1000);
